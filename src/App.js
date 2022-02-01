98f52136ac1073b904f1aa36ec2ef436e956c608
mmecham44 <mary.s.mecham@gmail.com>
2022-02-01 12:47:27 -0700

    Added dictionary js and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary📖</header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Mary Mecham</small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
