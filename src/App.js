588149b0d5a2461c09895a67e057a0db03a8e6cf
mmecham44 <mary.s.mecham@gmail.com>
2022-01-31 20:35:37 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Dictionary📖</header>
+    </div>
+  );
+}
+
+export default App;
