87ef8be118ab3c511336722fd8507e322e70162a
mmecham44 <mary.s.mecham@gmail.com>
2022-02-02 18:46:38 -0700

    added dictionary project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -7,13 +6,36 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <hi>Dictionary 📚</hi>
         </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="Pomeranian" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Mary Mecham</small>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/workshops/shecodes-responsive-42ca8482-5476-4b51-8a68-fb27a819d71e/projects/620140"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Mary Mecham
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/mmecham44/dictionary-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://infallible-lichterman-874322.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
         </footer>
       </div>
     </div>
