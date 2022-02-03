7f33c1d2e955e4153456ceaf0b3bf37087145d8e
mmecham44 <mary.s.mecham@gmail.com>
2022-02-02 19:36:49 -0700

    fixed alt text

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,12 @@ import "./Phonetic.css";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+      <a
+        href={props.phonetic.audio}
+        target="_blank"
+        rel="noreferrer"
+        alt="meaningful text"
+      >
         Listen
       </a>
       <span className="text">{props.phonetic.text}</span>
